b91ae3865d889be190f70676ee2e6678b0e00132
Evan Gillogley <evanjmg@gmail.com>
2015-10-08 10:59:38 +0100

    added travelpal logo

--- wp-content/themes/radiate/header.php
+++ wp-content/themes/radiate/header.php
@@ -36,7 +36,7 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="header-wrap clearfix">
 			<div class="site-branding">
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?> <img height="50" src="./travelpal-logo.png" alt="travelpal logo"></a></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</div>
 
