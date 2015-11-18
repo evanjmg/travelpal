e53c31717ef0ea255231fc621233ee8661db9bcf
Evan Gillogley <evanjmg@gmail.com>
2015-11-18 15:06:35 +0000

    changed footer2

--- wp-content/themes/radiate/footer.php
+++ wp-content/themes/radiate/footer.php
@@ -10,9 +10,14 @@
  */
 ?>
 </div><!-- .inner-wrap -->
-	</div><!-- #content --><footer id="colophon" class="site-footer" role="contentinfo"><div class="site-info"><?php do_action( 'radiate_credits' ); ?>© 2015 Travelpal | <a href="http://www.uktravelpal.com/privacy-policy/" target="_blank">Privacy Policy</a> | <a href="https://instagram.com/uktravelpal/">Instagram</a></div><!-- .site-info --></footer><!-- #colophon -->
-   <a href="#masthead" id="scroll-up"><span class="genericon genericon-collapse"></span></a>
-</div><!-- #page -->
-<?php wp_footer(); ?>
+	</div><!-- #content --><footer id="colophon" class="site-footer" role="contentinfo"><div class="site-info"><?php do_action( 'radiate_credits' ); ?>© 2015 Travelpal | <a href="http://www.uktravelpal.com/privacy-policy/" target="_blank">Privacy Policy</a> | <a href="https://instagram.com/uktravelpal/">Instagram</a></div>
+</div> <!-- #colophon -->  <a href="#masthead" id="scroll-up"><span class="genericon genericon-collapse"></span></a>
+	</footer>
+<!-- <?php wp_footer(); ?> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/js/foundation.min.js"></script>
+<script>
+jQuery('.entry-summary > .addthis_toolbox:first-child').next('.addthis_toolbox').hide();
+	jQuery('.byline').hide();
+</script>
 </body>
 </html>
